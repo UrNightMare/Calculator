15c65b7480963859f298fd1b66a870b4dc1c4d6b
George Sabanov <georgesabanov@MacBook-Pro-George-Sabanov.local>
2014-03-05 18:36:45 +0400

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by George Sabanov on 05.03.14.
+//  Copyright (c) 2014 Goga Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
