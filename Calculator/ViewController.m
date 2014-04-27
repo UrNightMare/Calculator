e7e5ed721299c3592bd252b0578184cfdcf146ed
UrNightMare <gogaloga777@gmail.com>
2014-04-27 17:37:03 +0400

    Вроде готова 1ая версия приложения

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -17,73 +17,85 @@
 
 - (void)viewDidLoad
 {
-    [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(check) userInfo:nil repeats:YES];
+    [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(check) userInfo:nil repeats:YES];
     [super viewDidLoad];
 	isNewEnter = YES;
 }
 -(void)check {
-    if ([self.textField.text length] >= 25){
-        self.textField.text = [self.textField.text substringWithRange:NSMakeRange(0,24)];
+   if ([self.textField.text length] >= 12){
+       self.textField.text = [self.textField.text substringWithRange:NSMakeRange(0,11)];
     }
 }
 
 - (IBAction)zeroPushed:(id)sender {
     if (isNewEnter)
     {
-        if ([self.textField.text length] < 2) {
-        self.textField.text = @"";
-        isNewEnter = NO;
+        if ([self.textField.text length] > 2) {
+            self.textField.text = @"";
+            isNewEnter = NO;
         }
-        if ([self.textField.text length] <= 2) {
-        self.textField.text = @"";
+        if ([self.textField.text length] <=2 >0) {
+            self.textField.text = @"";
             isNewEnter = YES;
         }
+        if ([self.textField.text length] ==1) {
+            self.textField.text = @"";
+            isNewEnter = NO;
+        }
+
     }
     NSString* CurrentnumberAsString = [NSString stringWithFormat:@"%li", (long)[sender tag]];
     NSString* currentValue = self.textField.text;
     currentValue = [currentValue stringByAppendingString:CurrentnumberAsString];
     self.textField.text = currentValue;
-
+    
 }
- - (IBAction)digitPushedBy:(id) sender {
-     
+- (IBAction)digitPushedBy:(id) sender {
+    
     if (isNewEnter)
     {
-    self.textField.text = @"";
-    isNewEnter = NO;
- }
+        self.textField.text = @"";
+        isNewEnter = NO;
+    }
     NSString* CurrentnumberAsString = [NSString stringWithFormat:@"%li", (long)[sender tag]];
     NSString* currentValue = self.textField.text;
     currentValue = [currentValue stringByAppendingString:CurrentnumberAsString];
     self.textField.text = currentValue;
- }
+}
 
 
 
-- (BOOL)textField:(UILabel *)aTextView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
-         // "Length of existing text" - "Length of replaced text" + "Length of replacement text"
-         NSInteger newTextLength = [aTextView.text length] - range.length + [text length];
-         
-         if (newTextLength > 10) {
-             // don't allow change
-             return NO;
-         }
-         self.textField.text = [NSString stringWithFormat:@"%li", (long)newTextLength];
-         return YES;
-     }
+- (BOOL)textField:(UILabel *)textField shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    // "Length of existing text" - "Length of replaced text" + "Length of replacement text"
+    NSInteger newTextLength = [textField.text length] - range.length + [text length];
+    
+    if (newTextLength > 12) {
+        // don't allow change
+        return NO;
+    }
+    self.textField.text = [NSString stringWithFormat:@"%li", (long)newTextLength];
+    return YES;
+}
 
 
 
 - (IBAction)pointPushed:(id)sender {
     NSRange range = [self.textField.text rangeOfString:@"."];
+    
     if (range.location != NSNotFound) {
         return;
+
     }
+    if ([self.textField.text length] ==1) {
+        isNewEnter = NO;
+    }
+    
     self.textField.text = [self.textField.text stringByAppendingString:@"."];
+    
 }
 
 - (IBAction)clearPushed:(id)sender {
-    self.textField.text = @"0";
+    self.textField.text = @"";
     isNewEnter = YES;
     lastSign = 0;
     lastValue = 0;
@@ -100,14 +112,36 @@
         {
             self.textField.text = new;
         }
-        else
-        {
-            isNewEnter = NO;
-            self.textField.text = @" ";
+        else        {
+            isNewEnter = YES;
+            self.textField.text = @"0";
         }
+    if (current <= 0) {
+        self.textField.text = @"0";
+    }
     
 }
 
+/*
+- (IBAction)LeftSlideRecognizer:(id)sender
+{
+    NSString * current = self.textField.text;
+    NSString * new = [current substringToIndex:[current length] ];
+    if ([new length] > 0)
+    {
+        self.textField.text = new;
+    }
+    else
+    {
+        isNewEnter = YES;
+        self.textField.text = @"0";
+    }
+    
+}
+
+
+
+*/
 
 
 
@@ -175,6 +209,8 @@
     
     lastValue = currentValue;
     isNewEnter = NO;
+
+        
     
     self.textField.text = [[NSNumber numberWithDouble:currentValue] stringValue];
 }
